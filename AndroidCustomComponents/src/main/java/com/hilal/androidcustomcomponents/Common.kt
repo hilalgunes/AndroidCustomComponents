069e329cf25d1f8dc2c7e17bb58dfb7ea3ade1ba
Hilal Güneş <hilal.gunes@detaysoft.com>
2024-04-18 15:26:55 +0300

    added class

--- AndroidCustomComponents/src/main/java/com/hilal/androidcustomcomponents/Common.kt
+++ AndroidCustomComponents/src/main/java/com/hilal/androidcustomcomponents/Common.kt
@@ -9,29 +9,30 @@ import androidx.appcompat.widget.AppCompatTextView
 /**
  * Created by Hilal Güneş on 18.04.2024
  */
+class Common {
+    fun createAndConfigurePopupItem(
+        context: Context,
+        title: String,
+        description: String,
+        explanation: String,
+        buttonText: String
+    ) {
+        val builder = AlertDialog.Builder(context)
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.popup_item, null)
+        builder.setView(view)
 
-fun createAndConfigurePopupItem(
-    context: Context,
-    title: String,
-    description: String,
-    explanation: String,
-    buttonText: String
-) {
-    val builder = AlertDialog.Builder(context)
-    val inflater = LayoutInflater.from(context)
-    val view = inflater.inflate(R.layout.popup_item, null)
-    builder.setView(view)
+        val alertDialog = builder.create()
+        alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        alertDialog.show()
 
-    val alertDialog = builder.create()
-    alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-    alertDialog.show()
+        view.findViewById<AppCompatButton>(R.id.button).setOnClickListener {
+            alertDialog.dismiss()
+        }
+        view.findViewById<AppCompatTextView>(R.id.title).text = title
+        view.findViewById<AppCompatTextView>(R.id.description).text = description
+        view.findViewById<AppCompatTextView>(R.id.explanation).text = explanation
+        view.findViewById<AppCompatButton>(R.id.button).text = buttonText
 
-    view.findViewById<AppCompatButton>(R.id.button).setOnClickListener {
-        alertDialog.dismiss()
     }
-    view.findViewById<AppCompatTextView>(R.id.title).text = title
-    view.findViewById<AppCompatTextView>(R.id.description).text = description
-    view.findViewById<AppCompatTextView>(R.id.explanation).text = explanation
-    view.findViewById<AppCompatButton>(R.id.button).text = buttonText
-
 }
